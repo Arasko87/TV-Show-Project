43438c9b8a3f0aaf93ec805d993124d68241871a
Aras Sulaiman <arasko87@gmail.com>
2020-07-02 09:20:53 +0100

    level  100 DONE

--- script.js
+++ script.js
@@ -3,10 +3,63 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
+//getAllEpisodes
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+        rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  let myElement = document.querySelector("#root");
+  let nameDiv = document.createElement("div");
+      myElement.appendChild(nameDiv);
+      nameDiv.className = "nameDiv";
+
+      episodeList.forEach(episode => {
+
+  let div1Contents = document.querySelector(".nameDiv");    
+  let divForBox = document.createElement("div");
+      div1Contents.appendChild(divForBox);
+      divForBox.className = "box";
+      
+
+  let h3 = document.createElement("h3");
+      divForBox.appendChild(h3);
+      h3.className = "h1Class";
+      h3.innerText = episode.name.concat(innerText =` - S${addZeroPrefix (episode.season)}E${addZeroPrefix(episode.number)}`);
+     
+  let img = document.createElement("img");
+  divForBox.appendChild(img);
+      img.src = episode.image.medium;
+       
+  let para = document.createElement("p");
+  divForBox.appendChild(para);
+      para.className = "paragraph";
+      para.innerHTML = episode.summary;
+
+  let a = document.createElement('a');  
+  let link = document.createTextNode("Watch on TVMaze.com"); 
+      a.appendChild(link); 
+      a.className = "link" 
+      a.href = episode.url;  
+      divForBox.appendChild(a);
+
+      div1Contents.appendChild(divForBox);
+});
+      
 }
 
+
+function addZeroPrefix (num){
+
+  if (num < 10 ){
+    return "0" + num ;
+  }
+  else{
+    return num;
+  }
+}
+
+
+
+
 window.onload = setup;
+
---
+++ test.js
@@ -0,0 +1,46 @@
+
+
+//=================================================================
+
+
+
+/*
+        episodeList.forEach(function (contents) {
+  let allContent = document.createElement("div");
+      allContent.className = "allContent";
+      rootElem.appendChild(allContent);
+      }
+
+      ==========================
+//the link to the Maze site.
+  var a = document.createElement('a');  
+  var link = document.createTextNode(`Maze Original website`); 
+         a.appendChild(link);  
+           a.href = "https://www.tvmaze.com/";  
+           document.body.appendChild(a);  
+//==============
+
+// Table contents.
+
+  let table = document.createElement("table");
+  rootElem.appendChild(table);
+  table.className = "tableContent";
+  //let row = table.insertRow();
+  for (let i = 0; i< episodeList.length; i++){
+    let row = table.insertRow();
+    row.className = "tableRow";
+      cell1 = row.insertCell();
+      cell1.innerText = episodeList[i].name.concat(innerText =` - S0${episodeList[i].season}E0${episodeList[i].number}`);
+    let img1 = row.insertCell();
+    let img = document.createElement("img");
+          img.src = episodeList[i].image.medium;
+          img1.appendChild(img);
+        row.insertCell().innerHTML = episodeList[i].summary;
+
+  }
+
+}
+}}
+*/
+
+
